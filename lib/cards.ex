3ca7389e82e44a87c261b438309a6c085b391789
joey-mbpr <xjkit54780@gmail.com>
2017-03-24 09:37:30 +0800

    Update example code

--- lib/cards.ex
+++
@@ -1,7 +0,0 @@
-defmodule Cards do
-
-  def create_deck do
-    ["Ace", "Two", "Three"]
-  end
-
-end
---
+++ lib/cards.exs
@@ -0,0 +1,15 @@
+defmodule Cards do
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def greeting do
+    IO.puts "Hello, const #{:bowwow}"
+  end
+
+  def _hideMe do
+    IO.puts "I am hiding..."
+  end
+
+end
